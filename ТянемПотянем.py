2425e43c69c897c8aaafb696fea5d8c92d907aa4
Maria-vin <118813125+Maria-vin@users.noreply.github.com>
2022-12-21 22:51:12 +0300

    Add files via upload

--- ТянемПотянем.py
+++ ТянемПотянем.py
@@ -1,6 +1,6 @@
 from tkinter import *
 from tkinter import messagebox
-def calculate_bmi():
+def calculate():
     buhta = float(weight.get()) / 3
     povt = float(weight.get()) / 3
     kross = float(weight.get()) / 1.5
@@ -9,27 +9,26 @@ def calculate_bmi():
     if sum > 0:
         messagebox.showinfo('затраты', f'Примерные затраты материала = {sum} рублей')
     else:
-        messagebox.showinfo('затраты',  f'Вы не ввели данные')
+        messagebox.showinfo('затраты',  f'Вы не корректно ввели данные')
 def clear():
     weight.delete("0", "end")
 window = Tk()
 window.title('Примерные затраты на материал')
-window.geometry('700x100')
-window["bg"] = "lemon chiffon"
+window.geometry('600x100')
 
 
-name = Label(window, text="Рассчитать затрат", bg="lemon chiffon", fg="black", width=28, font=('helvetica', 15, 'bold'), compound=RIGHT)
-name.grid(row=0, column=0)
+name = Label(window, text="Рассчитать затрат")
+name.grid(row=0, column=1)
 
-weight = Label(window, text="Введите протяженность трассы(в км)", bg="lemon chiffon", fg="black", font=6)
+weight = Label(window, text="Введите протяженность трассы(в км)")
 weight.grid(row=1, column=1)
 
-weight = Entry(window, width=28, font=6, bg="navajo white", bd=5)
+weight = Entry(window)
 weight.grid(row=1, column=2)
 
-cal_btn = Button(window, text='Рассчитать', bg="navajo white", command=calculate_bmi, bd=5, width=15, font=6)
+cal_btn = Button(window, text='Рассчитать', command=calculate)
 cal_btn.grid(row=3, column=1)
-clear_btn = Button(window, fg="black", text="Сброс", bg="navajo white", command=clear, bd=5,  width=15, font=6)
+clear_btn = Button(window, text="Сброс", command=clear)
 clear_btn.grid(row=3, column=2)
 
 window.mainloop()
